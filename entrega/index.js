094e190812f0897667f5e4f3b0664b4626881a2f
Allana Caldas <allanacaldas@gmail.com>
2020-09-23 19:04:00 -0300

    Adicionando outro possível meio de resolução através do findIndex

--- entrega/index.js
+++ entrega/index.js
@@ -5,18 +5,23 @@ const app = express();
 const mulheresIncriveis = require('./mulheresIncriveis.json')
 
 /** GET **/
-app.get('/mulheresIncriveis', function(request, response){
+app.get('/mulheresIncriveis', function (request, response) {
     response.json(mulheresIncriveis)
 });
 
 /**GET com ID **/
-app.get('/mulheresIncriveis/:id', function(request, response){
+app.get('/mulheresIncriveis/:id', function (request, response) {
     const id = request.params.id;
-    const mulherSelecionada = mulheresIncriveis.find(mulher => mulher.id == id)
+    const mulherSelecionada = mulheresIncriveis.find(mulher => mulher.id == id);
     //console.log(mulherSelecionada)
     response.json(mulherSelecionada)
+
+    /***** Outro modo de resolução também com o findIndex ******/
+    //const mulherSelecionada = mulheresIncriveis.findIndex(mulher => mulher.id == id)
+    //response.json(mulheresIncriveis[mulherSelecionada])//
+
 });
 
-app.listen(porta, function(){
-    console.log( `O servidor está rodando na porta ${porta} e viva o empoderamento feminino`)
+app.listen(porta, function () {
+    console.log(`O servidor está rodando na porta ${porta} e viva o empoderamento feminino`)
 })
